1c6990fd2ed377518cb0b8e27c2d29c06694e15b
Krish90s <krishnanvenkat4@gmail.com>
2024-01-15 16:29:50 +0530

    get data error

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import { Container, Flex, Link, Box } from "@radix-ui/themes";
 
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Layout from "./components/Layout";
-import NoMatch from "./pages/NoMatch";
 import axios from "axios";
 import Cookies from "js-cookie";
 
 const App = () => {
-	const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(false);
 
-	useEffect(() => {
-		const fetchCurrentUser = async () => {
-			try {
-				const response = await axios.get(
-					"http://localhost:3000/api/v1/auth/google/success"
-				);
-				console.log("CurrentUser", response);
-				const accessToken = Cookies.get("access_token");
-				console.log("accessToken", accessToken);
-			} catch (error) {
-				console.log("error", error);
-			}
-		};
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/auth/google/success"
+        );
+        console.log("CurrentUser", response);
+        const accessToken = Cookies.get("access_token");
+        console.log("accessToken", accessToken);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
 
-		fetchCurrentUser();
-	}, []);
+    fetchCurrentUser();
+  }, []);
 
-	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/home" element={<Home />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="border-b mb-5 px-5 py-3">
+        <Box>
+          <Flex justify="between">
+            <Flex align="center" gap="3">
+              <NavLink to="/home">Home</NavLink>
+              <NavLink to="/login">Login</NavLink>
+            </Flex>
+          </Flex>
+        </Box>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,24 @@
 import { Button } from "@radix-ui/themes";
 import React from "react";
 import { Link } from "react-router-dom";
+import { Grid, Flex, Box } from "@radix-ui/themes";
 
 const Login = () => {
-	return (
-		<div>
-			<Link to="http://localhost:3000/api/v1/auth/google">
-				<Button color="blue" variant="outline">
-					Continue with Google
-				</Button>
-			</Link>
-		</div>
-	);
+  const handleGoogleLogin = () => {
+    window.open("http://localhost:3000/api/v1/auth/google", "_self");
+  };
+  return (
+    <Box height="100%" className="px-5">
+      {/* <Link to="http://localhost:3000/api/v1/auth/google">
+        <Button color="blue" variant="outline">
+          Continue with Google
+        </Button>
+      </Link> */}
+      <Button color="blue" variant="outline" onClick={handleGoogleLogin}>
+        Continue with Google
+      </Button>
+    </Box>
+  );
 };
 
 export default Login;
